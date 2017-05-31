faeef6e0e01902caaa8b25c26dad8e7019b95b51
Blanca Azucena López Garduño <blankazucenalg@gmail.com>
2017-05-30 19:20:59 -0500

    Update readme instructions

--- README.md
+++ README.md
@@ -1,16 +1,50 @@
 # dboxjs examples
 
 ## Instalación
-Las dependencias del proyecto se instalaron usando NPM, que se encuentra dentro de la instalación de Node.js [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+Primero que nada, clona o descarga este proyecto. 
 
-Debido a que se consumen archivos, se requiere ```http-server``` [https://github.com/indexzero/http-server](https://github.com/indexzero/http-server)
+Las dependencias del proyecto se instalaron usando NPM, que se encuentra dentro de la instalación de Node.js 
 
-1. Instalar dependencias
+### No tengo NPM, ¿qué hago?
+
+Instala la versión adecuada de Node.js para tu computadora, que se encuentra disponible en [https://nodejs.org/en/download/](https://nodejs.org/en/download/)
+
+Si tienes una distribución GNU/Linux, puedes hacerlo a través del gestor de paquetes [https://nodejs.org/en/download/package-manager/](https://nodejs.org/en/download/package-manager/)
+
+### Ok, ya instalé Node, ¿y ahora?
+
+Debido a que se consumen archivos, se requiere montar una instancia de un servidor para poder consumir recursos (por cuestiones de seguridad de JavaScript) 
+
+Recomendamos instalar ```http-server``` por practicidad [https://github.com/indexzero/http-server](https://github.com/indexzero/http-server)
+
+#### Instalación en UNIX
+```
+sudo npm install -g http-server
+```
+
+### Instalación en Windows
+Debes ejecutar el programa CMD en modo de administrador (Botón de Windows, CMD -> click derecho -> Ejecutar como administrador)
+
+Una vez que se abre la terminal, ejecutar el comando
+```
+npm install -g http-server
+``` 
+
+## Ok, ya tengo NPM y una instancia de servidor, ¿ahora como visualizo el proyecto?
+
+### 1. Instalar dependencias
+
+Debes ir en la linea de comandos (terminal, CMD) a la carpeta donde descargaste el proyecto
+```
+cd <-path to folder->
+```
+
+Una vez que estás ahí, instalas las dependencias del proyecto
 ```
 npm install 
 ```
 
-2. Iniciar el proyecto (estando en la carpeta de este proyecto)
+### 2. Iniciar el proyecto (estando en la carpeta de este proyecto)
 ```
 http-server .
 ```
--- js/dbox_examples.js
+++ js/dbox_examples.js
@@ -1,3 +1,7 @@
+this.changeData = function(){
+  alert('no molestes');
+}
+
 // Scatter
 /***************************
  *        SCATTER
@@ -16,17 +20,23 @@ var config = {
   }
 }
 
+// raw 
+// csv
+// tsv
+// json
+// carto 
+
 var chart = dbox.chart(config)
   .data({
     'tsv': 'assets/data/data.tsv'
   })
   .layer(dbox.scatter)
-  .x('sepalWidth')
-  .y('sepalLength')
-  .color('species')
-  .tip(function(d){
-    return d.color + ' (' + d.x + ',' + d.y + ')';
-  })
+    .x('sepalWidth')
+    .y('sepalLength')
+    .color('species')
+    .tip(function(d){
+      return d.color + ' (' + d.x + ',' + d.y + ')';
+    })
   .end()
   .draw();
 
@@ -36,38 +46,38 @@ var chart = dbox.chart(config)
 /***************************
  *        TREEMAP
  ***************************/
-// var colors = ['#562B4F', '#742C6A', '#B23733', '#DC8933'];
-// 
-// var config = {
-//   'size': {
-//     'width': 600,
-//     'height': 600,
-//     'margin': {
-//       top: 0,
-//       right: 0,
-//       bottom: 0,
-//       left: 0
-//     },
-//   },
-//   'xAxis': {
-//     'enabled': false
-//   },
-//   'yAxis': {
-//     'enabled': false
-//   }
-// };
-// 
-// dbox.chart(config)
-//   .bindTo('#treemap')
-//   .data({
-//     'tsv': 'assets/data/test.tsv'
-//   })
-//   .layer(dbox.treemap)
-//   .nestBy(['variable', 'category', 'subcategory'])
-//   .size('number')
-//   .format(',.0f')
-//   .end()
-//   .draw();
+var colors = ['#562B4F', '#742C6A', '#B23733', '#DC8933'];
+
+var config = {
+  'size': {
+    'width': 600,
+    'height': 600,
+    'margin': {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    },
+  },
+  'xAxis': {
+    'enabled': false
+  },
+  'yAxis': {
+    'enabled': false
+  }
+};
+
+dbox.chart(config)
+  .bindTo('#treemap')
+  .data({
+    'tsv': 'assets/data/test.tsv'
+  })
+  .layer(dbox.treemap)
+    .nestBy(['variable', 'category'])
+    .size('number')
+    .format(',.0f')
+  .end()
+  .draw();
 
 
 
@@ -75,54 +85,53 @@ var chart = dbox.chart(config)
 /***************************
  * MAPA DE ESTADOS DE MÉXICO
  ***************************/
-// var config = {
-//   'bindTo': '#map',
-//   'size': {
-//     'width': 900,
-//     'height': 590,
-//     'margin': {
-//       top: 0,
-//       right: 0,
-//       bottom: 0,
-//       left: 50
-//     },
-//     'translateX': 45,
-//     'scale': 1.3
-//   },
-//   'xAxis': {
-//     enabled: false
-//   },
-//   'yAxis': {
-//     enabled: false
-//   },
-//   'plotOptions': {
-//     'map': {
-//       'geo': 'mexico',
-//       'geoDivision': 'states',
-//       'quantiles': {
-//         'buckets': 5,
-//         'colors': ['#e3f0cd', '#cae2a1', '#b1d375', '#98c449', '#7fb61d'] 
-//       }
-//     }
-//   },
-//   'events': {
-//     'load': function(chart) {}
-//   }
-// }
-// 
-// var mapa = dbox.chart(config)
-//   .data({
-//     'csv': 'assets/data/datos_estados.csv'
-//   })
-//   .layer(dbox.map)
-//   .z('value')
-//   .tip(function(d) {
-//     console.log(d, d3.select(d));
-//     var name = d.properties.state_name + '<br>' + (d3.select('#state-' + d.id).attr('data-total'));
-//     return name;
-//   })
-//   .end()
-//   .draw();
+var config = {
+  'bindTo': '#map',
+  'size': {
+    'width': 900,
+    'height': 590,
+    'margin': {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 50
+    },
+    'translateX': 45,
+    'scale': 1.3
+  },
+  'xAxis': {
+    enabled: false
+  },
+  'yAxis': {
+    enabled: false
+  },
+  'plotOptions': {
+    'map': {
+      'geo': 'mexico',
+      'geoDivision': 'states',
+      'quantiles': {
+        'buckets': 5,
+        'colors': ['#e3f0cd', '#cae2a1', '#b1d375', '#98c449', '#7fb61d'] 
+      }
+    }
+  },
+  'events': {
+    'load': function(chart) {}
+  }
+}
+
+var mapa = dbox.chart(config)
+  .data({
+    'csv': 'assets/data/datos_estados.csv'
+  })
+  .layer(dbox.map)
+    .z('value')
+    .tip(function(d) {
+      var name = d.properties.state_name + '<br>' + (d3.select('#state-' + d.id).attr('data-total'));
+      return name;
+    })
+  .end()
+  .draw();
   
   
   
